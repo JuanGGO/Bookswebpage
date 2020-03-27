ae957fdf4e31d4f5d8e35add7d964e953083546b
Juan <juan@pop-os.localdomain>
2020-03-26 18:25:34 -0700

    Added up to table creation

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,7 @@ import os
 from flask import Flask, session
 from flask_bootstrap import Bootstrap
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
@@ -16,11 +15,12 @@ if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set!!")
 
 bootstrap = Bootstrap(app)
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
 
 from .main import main as main_blueprint
 app.register_blueprint(main_blueprint)
 
 from .auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
+
+from .models import db
+db.init_app(app)
--- app/models.py
+++ app/models.py
@@ -1,1 +1,24 @@
-# TODO: Construct the relevant tables for the website
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, nullable=False, index=True)
+    username = db.Column(db.String, nullable=False, unique=True, index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a readable property")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
